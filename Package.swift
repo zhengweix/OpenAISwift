9bc1d940eab5397e4b0eda4f20f71a47cc47c768
zhengweix <zheng.wei@famewell.com>
2023-09-30 22:19:22 -0700

    updated 230930

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "openai-swift",
+    name: "OpenAISwift",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "openai-swift",
-            targets: ["openai-swift"]),
+            name: "OpenAISwift",
+            targets: ["OpenAISwift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "openai-swift"),
+            name: "OpenAISwift"),
         .testTarget(
-            name: "openai-swiftTests",
-            dependencies: ["openai-swift"]),
+            name: "OpenAISwiftTests",
+            dependencies: ["OpenAISwift"]),
     ]
 )
